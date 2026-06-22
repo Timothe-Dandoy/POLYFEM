aef23be60e718b11a86442249105aeb2df438840
Timothé Dandoy <96069508+Timothe-Dandoy@users.noreply.github.com>
2026-06-22 10:57:45 +0200

    Add files via upload

---
+++ scripts/One_Species/2_build_VCF_chrZ/merge_all_vcf_chr_Z.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -l
+#SBATCH -J merge_VCF_chr_Z
+#SBATCH -o ../../logs/merge_vcf_z.out
+#SBATCH -e ../../logs/merge_vcf_z.out
+#SBATCH -p workq
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=256G
+#SBATCH --mail-type=FAIL
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Variables
+
+#data
+species=$1
+nickname=$(echo ${species} | cut -d'_' -f2 | cut -c1-3)
+chr=NC_041330.1   # Chromosome Z
+START_IDX=1   
+END_IDX=509  
+
+#Inputs
+dir_tmp=../../../tmp/${species}/Files_genet/vcf/
+
+#Temporary
+merged_tmp=${dir_tmp}${nickname}_${chr}_vcf_unsorted.vcf.gz
+
+#Output
+merged_sorted=${dir_tmp}${nickname}_${chr}_vcf_merged.vcf.gz
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 1. Generate the list of all sub region VCFs
+echo
+echo "step 1"
+echo
+
+vcf_list=$(ls ${dir_tmp}*.vcf | sort -V | sed -n "${START_IDX},${END_IDX}p")
+
+# Ensure there are enough files
+if [[ -z ${vcf_list} ]]; then
+  echo "No VCF files found in the specified range: $START_IDX to $END_IDX"
+  exit 1
+fi
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 2. Conctenate, sort and index VCFs in one single VCF for the whole chromosome Z
+echo
+echo "step 2"
+echo
+
+module purge
+module load bioinfo/Bcftools
+
+bcftools concat -Oz -o ${merged_tmp} ${vcf_list}
+bcftools sort -Oz ${merged_tmp} -o ${merged_sorted}
+bcftools index ${merged_sorted}
+
+rm ${merged_tmp}
+
---
+++ scripts/One_Species/4_GWAS/4_GWAS.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -l
+#SBATCH -J GWAS
+#SBATCH -o ../../logs/GWAS.out
+#SBATCH -e ../../logs/GWAS.out
+#SBATCH -p workq
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=512G
+#SBATCH --mail-type=FAIL
+#SBATCH --time=72:00:00
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Variables
+
+# Need to give the name of the species you want to study => Ex: P_guadarramae, P_hispanicus, ...
+species=$1
+nickname=$(echo ${species} | cut -d'_' -f2 | cut -c1-3)
+
+#Inputs
+File_Pheno=../../../tmp/${species}/Files_pheno/${nickname}_phenotype.yquant
+File_Pop=../../../tmp/${species}/Files_pheno/${nickname}_population.txt
+File_BamList=../../../tmp/${species}/Files_pheno/${nickname}_bam_list_filtered.fofn
+
+#Temporary
+prefix_out=../../../tmp/${species}/Files_genet/${nickname}_out_gwas
+out_GWAS=../../../tmp/${species}/Files_genet/${nickname}_out_gwas.lrt0.gz
+
+#Output
+prefix_res=../../../results/${species}/GWAS/${nickname}_gwas_
+
+#Secondary script
+Analyse_R=../Secondary/Analyse_GWAS.R
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 1. GWAS Computation
+echo
+echo "step 1"
+echo
+
+module load bioinfo/ANGSD/0.940
+
+angsd -yQuant $File_Pheno \
+      -doAsso 2 \
+      -cov $File_Pop \
+      -GL 1 \
+      -doPost 1 \
+      -out $prefix_out \
+      -doMajorMinor 1 \
+      -doMaf 1 \
+      -SNP_pval 1e-6 \
+      -Pvalue 1 \
+      -bam $File_BamList \
+      -P 8 
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 2. GWAS Analysis
+echo
+echo "step 2"
+echo
+
+module load statistics/R/4.5.0
+module load compilers/gcc/15.1.0
+
+Rscript $Analyse_R $out_GWAS $species $prefix_res
+
+
+
