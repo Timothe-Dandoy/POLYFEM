e0fb8e808d3f7c41d329617abff5bded5f9234fa
Timothé Dandoy <96069508+Timothe-Dandoy@users.noreply.github.com>
2026-08-20 11:37:03 +0200

    Add build_input_files.R for BAM file generation
    Create R script to build input files from Excel data.

---
+++ scripts/All_Species/Secondary/build_input_files.R
@@ -0,0 +1,25 @@
+#-----------------------------------------------------------------
+# Packages
+#-----------------------------------------------------------------
+
+library(readxl)
+
+#-----------------------------------------------------------------
+# Data
+#-----------------------------------------------------------------
+
+table_all <- commandArgs(trailingOnly = TRUE)[1]
+bam_list  <- commandArgs(trailingOnly = TRUE)[2]
+
+BD = read_xlsx(path =table_all)
+BD <- BD[!is.na(BD$'genetic data') & BD$'genetic data' == "Y",]
+
+#-----------------------------------------------------------------
+# Scripts
+#-----------------------------------------------------------------
+
+IDs = BD$Sample_ID
+
+# Lists of bam files 
+Bam = paste0('../../../../bam_analysis/',IDs,'.bam')
+writeLines(Bam, bam_list)
